dfe3447e1036b5a7e43e076279f6221af0dde782
alta1212 <nguyen.anh@hcl.com>
2022-11-12 15:59:41 +0700

    a

--- src/app/auth/khumphaibae/khumphaibae.component.ts
+++ src/app/auth/khumphaibae/khumphaibae.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-khumphaibae',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KhumphaibaeComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private http: HttpClient
+
+  ) {
 
     // background: #1b1b1b;
     // color: white;
@@ -16,7 +20,40 @@ export class KhumphaibaeComponent implements OnInit {
     // text-align: center;
       document.body.style.backgroundColor='#332851'
    }
+  readonly BaseURI="https://bae-back.alta1212.repl.co/";
 
+   addnew(a){
+    const headers = new HttpHeaders()
+    .set('Authorization', 'my-auth-token')
+    .set('Content-Type', 'application/json');
+    this.http.post(this.BaseURI+"newd",JSON.stringify({'pass':a}),{responseType: 'json', headers: headers}).subscribe(
+      (res:any) => {
+        console.log(res)
+        localStorage.setItem('token',res)
+        alert('Đúng bae rùi')
+        
+        window.location.href='heart/main'
+  
+      },
+      err => {
+        if(err.status!==401)
+        alert('Hình như có lũi rùi em chụp mh gửi anh nhá \n'+JSON.stringify(err))
+        else
+        {
+          alert('nah not bae ')
+        }
+      }
+    );
+
+
+
+
+
+
+
+
+   }
+   
   ngOnInit(): void {
     
 
